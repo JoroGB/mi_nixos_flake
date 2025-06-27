7335c9d852f8a7a05a45d22da557fcd1ad62a48f
JoroGB <134667974+JoroGB@users.noreply.github.com>
2025-06-26 21:03:57 -0600

    second commit

--- my_nixos_gnm/configuration.nix
+++ my_nixos_gnm/configuration.nix
@@ -55,7 +55,6 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.joronix = {
-     password = "fungy2005";
      isNormalUser = true;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
