14cbd10a8e501b348cfcc40e25550b0c65e28c60
JordyCommit <134667974+JoroGB@users.noreply.github.com>
2025-06-27 21:04:13 -0600

    Update configuration.nix

--- my_nixos_gnm/configuration.nix
+++ my_nixos_gnm/configuration.nix
@@ -2,10 +2,10 @@
  
 
 {
-#  imports =
-#   [ # Include the results of the hardware scan.
-#      ./hardware-configuration.nix
-#   ];
+  imports =
+   [ # Include the results of the hardware scan.
+      ../hardware-configuration.nix
+   ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
