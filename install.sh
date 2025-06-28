590406d900befaf0e8aae0076b991ed1e98a4bd9
JoroGB <134667974+JoroGB@users.noreply.github.com>
2025-06-28 16:27:17 -0600

    se modifico un poco el archivo install.sh

--- install.sh
+++ install.sh
@@ -6,32 +6,35 @@ echo "Por favor escriba el nombre de un disco";
 read -r disco;
 
 #Parted disk
-parted /dev/"$disco" -- mklabel gpt
-parted /dev/"$disco" -- mkpart root ext4 512MB 
 
-# Parted Boot
+#Create table
+parted /dev/"$disco" -- mklabel gpt
+
+# ESP EFI
 parted /dev/"$disco"  -- mkpart ESP fat32 1MB 512MB
 parted /dev/"$disco"  -- set 2 esp on
+
+# root partition
+parted /dev/"$disco" -- mkpart root ext4 512MB 
+
 echo "Particion exitosa"
-echo
-echo
+
+
 
 echo "Formateando"
 mkfs.ext4 -L nixos /dev/"$disco"1
 mkfs.fat -F 32 -n boot /dev/"$disco"2
 echo
-echo
 echo "Formateo exitoso"
 echo
 echo
 echo "Montando particiones"
-echo
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
 echo "particiones montadas"
 echo
-echo "Escriba el nombre de la configuracion"
+echo "Escriba el nombre de la configuracion flake"
 read -r configuration;
 
 nixos-install --flake ./flake.nix#$configuration
