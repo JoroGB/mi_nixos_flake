4937c0392deeed4b2dfb7283f14bb291f6c79f2e
JoroGB <134667974+JoroGB@users.noreply.github.com>
2025-11-02 15:07:19 -0600

    save to check

---
+++ home_hp/home.nix
@@ -0,0 +1,86 @@
+{ config, pkgs, ... }:
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+  configs = {
+    hypr = "hypr";
+    nvim = "nvim";
+    wofi = "wofi";
+    rofi = "rofi";
+    foot = "foot";
+    waybar = "waybar";
+  };
+in
+{
+  imports = [
+    ./modules/theme.nix
+  ];
+
+  home.username = "joronix";
+  home.homeDirectory = "/home/joronix";
+  home.stateVersion = "25.05";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use hyprland btw by joronix";
+      nrs = "sudo nixos-rebuild switch --flake ~/mi_nixos_flake#nixos_pc_hp";
+      vim = "nvim";
+    };
+    initExtra = ''
+      export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
+      nitch
+    '';
+    profileExtra = ''
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+          exec uwsm start -S hyprland-uwsm.desktop
+      fi
+    '';
+  };
+  environment.variables = {
+    # Necesarias para Hyprland + NVIDIA
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    LIBVA_DRIVER_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_EGL_STREAMS = "0"; # fuerza GBM, evita fallback a EGLStreams
+  };
+
+  home.packages = with pkgs; [
+      discord-ptb
+      warp-terminal
+      pomodoro
+
+      brave
+      google-chrome
+      vivaldi
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    wofi
+    nitch
+    rofi
+    pcmanfm
+    (pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        (nix-search-tv.overrideAttrs {
+          env.GOEXPERIMENT = "jsonv2";
+        })
+      ];
+      text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
+    })
+  ];
+
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
+}
