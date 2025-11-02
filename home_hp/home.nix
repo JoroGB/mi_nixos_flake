85b771f9ef6c59167c1ec0390547bd97f8972fc9
JoroGB <134667974+JoroGB@users.noreply.github.com>
2025-11-02 15:28:17 -0600

    save status

--- home_hp/home.nix
+++ home_hp/home.nix
@@ -64,16 +64,16 @@ in
     rofi
     pcmanfm
     go
-    (pkgs.writeShellApplication {
-      name = "ns";
-      runtimeInputs = with pkgs; [
-        fzf
-        (nix-search-tv.overrideAttrs {
-          env.GOEXPERIMENT = "jsonv2";
-        })
-      ];
-      text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
-    })
+    # (pkgs.writeShellApplication {
+    #   name = "ns";
+    #   runtimeInputs = with pkgs; [
+    #     fzf
+    #     (nix-search-tv.overrideAttrs {
+    #       env.GOEXPERIMENT = "jsonv2";
+    #     })
+    #   ];
+    #   text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
+    # })
   ];
 
   xdg.configFile = builtins.mapAttrs
